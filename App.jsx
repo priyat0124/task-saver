2b3632d43b16263a760b2565de98a82385df486c
priyat0124 <priyat0124@gmail.com>
2024-06-12 20:44:53 +0530

    ui src

---
+++ App.jsx
@@ -0,0 +1,51 @@
+//App.js 
+
+import React from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Todo from './todo'; 
+
+
+function App() { 
+const headStyle = { 
+	textAlign: "center", 
+} 
+return ( 
+	<div> 
+	<h1 style={headStyle}>TASK SAVER</h1> 
+	<BrowserRouter> 
+		<Routes> 
+		<Route path='/' element={<Todo/>}></Route> 
+		</Routes> 
+	</BrowserRouter> 
+	</div> 
+); 
+} 
+
+export default App;
+
+
+
+/*import React from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Todo from './Todo'; 
+
+
+function App() { 
+const headStyle = { 
+  textAlign: "center", 
+} 
+return ( 
+  <div> 
+  <h1 style={headStyle}>Todo List</h1> 
+  <BrowserRouter> 
+    <Routes> 
+    <Route path='/' element={<Todo/>}></Route> 
+    </Routes> 
+  </BrowserRouter> 
+  </div> 
+); 
+} 
+
+export default App; */
